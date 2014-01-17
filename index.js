4810fac6fad29679955ebffafba211ed19fb3a35
Andrei Karpushonak <karpushonak@gmail.com>
2014-01-17 01:14:03 +0100

    Check nested attribute which are actually objects

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var deleteKeysFromObject = function (object, keys) {
   var keysToDelete;
 
   // do not modify original object
-  var finalObject = _.clone(object)
+  var finalObject = _.clone(object, isDeep=true)
 
   if (typeof finalObject === 'undefined') throw new Error('undefined is not a valid object.')
   if (arguments.length != 2) throw new Error("provide two parameters: object and list of keys")
@@ -24,7 +24,10 @@ var deleteKeysFromObject = function (object, keys) {
         if (elem === prop) {
           var deleteResult = delete finalObject[prop]
         } else {
-          finalObject[prop] = deleteKeysFromObject(finalObject[prop], keysToDelete)
+          // check nested attributes, if it's an object, and not array (thank you, Javascript)
+          if (typeof(finalObject[prop]) == 'object' && !Array.isArray(finalObject[prop])) {
+            finalObject[prop] = deleteKeysFromObject(finalObject[prop], keysToDelete)
+          }
         }
     }
   })
